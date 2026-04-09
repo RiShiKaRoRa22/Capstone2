802d12db186e2ac92a55e02c2c828c9c4cc73a91
Rishika Arora <rishikarora2006@gmail.com>
2026-04-09 09:44:32 +0530

    .

--- day4_problems/InvertBinTree.java
+++ day4_problems/InvertBinTree.java
@@ -33,6 +33,20 @@ public class InvertBinTree{
         inorder(root.right);
 
     }
+    public static void main(String args[]){
+        InvertBinTree obj= new InvertBinTree();
+        TreeNode root= new TreeNode(1);  
+        root.left= new TreeNode(2);
+        root.right=new TreeNode(3);
+        root.left.left=new TreeNode(4);
+        root.right.right= new TreeNode(5);
 
+        System.out.print("og tree: ");
+        obj.inorder(root);
+        obj.invertTree(root);
+        System.out.println("inverted tree: ");
+        obj.inorder(root);
+
+    }
 
 }
